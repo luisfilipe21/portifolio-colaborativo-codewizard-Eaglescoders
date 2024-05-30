e5f2e80bfdc73a69dd42a105a0eb32c985e27388
Davi Santanna <davicalvalcanti@gmail.com>
2024-05-29 22:29:58 -0300

    CREATE: "created all files and organized all files"

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <main className="flex flex-col">
+    <span>[Coloque aqui o hero]</span>
+      <span>[Coloque aqui a sessão de benefits]</span>
+      <span>[Coloque aqui a sesão de tech-and-lang]</span>
+      <span>[Coloque aqui a sesão de team]</span>
+      <span>[Coloque aqui a sesão de aboutProject]</span>
+      <span>[Coloque aqui a sesão de coments]</span>
+      <span>[Coloque aqui o cabecalho]</span>
+    </main>
+  </React.StrictMode>
+);
