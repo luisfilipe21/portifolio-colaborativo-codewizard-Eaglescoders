9e4542af4356d000ed8d4612a8947e80d59c46e9
Alerrando <alerrando2@gmail.com>
2024-05-31 20:11:31 -0300

    feat: add about project header and add Mulish font

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { AboutProject } from "./components/about-project";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <main className="flex flex-col">
-    <span>[Coloque aqui o hero]</span>
+      <span>[Coloque aqui o hero]</span>
       <span>[Coloque aqui a sessão de benefits]</span>
       <span>[Coloque aqui a sesão de tech-and-lang]</span>
       <span>[Coloque aqui a sesão de team]</span>
-      <span>[Coloque aqui a sesão de aboutProject]</span>
+      <AboutProject />
       <span>[Coloque aqui a sesão de coments]</span>
       <span>[Coloque aqui o cabecalho]</span>
     </main>
