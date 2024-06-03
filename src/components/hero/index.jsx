b8e6b214407e8362e46f3661227f260ac956bfba
Luis Filipe Guimarães Costa <luisfilipegc@hotmail.com>
2024-06-03 15:51:14 -0300

    feat: estilização da página finalizada. Próximo passo é trabalhar na responsividade

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,50 +1,58 @@
+import react from "../../assets/react.svg";
+import tailwind from "../../assets/tailwind.svg";
+import js from "../../assets/js.svg";
+import vite from "../../assets/vite.svg";
+
+
 export const HeroSection = () => {
     return (
-        <section className="w-full h-screen">
-            <div className="flex justify-center items-center ">
-                <div className="w-1/2 h-screen p-12">
-                    <div className="flex gap-4 h-3/4">
-                        <div className="min-w-40 h-full flex items-center">
-                            <div className="bg-black w-full h-48 rounded-2xl">
+        <section className="w-full h-screen max-w-5xl mx-auto my-auto bg-blue-900">
+            <div className="flex justify-between h-full w-full p-8">
+                <div className="h-full flex">
+                    <div className="flex gap-4 items-center">
+                        
+                        <div className="flex ">
+                            <div className="w-28 h-40 bg-black rounded-3xl mb-32">
 
                             </div>
                         </div>
-                        <div className="min-w-40 h-full flex flex-col mt-6">
-                            <div className="bg-black w-full h-48 rounded-2xl mb-4">
+                        <div className="flex flex-col mb-64">
+                            <div className="w-28 h-40 bg-black rounded-3xl mb-4">
 
                             </div>
-                            <div className="bg-black w-full h-48 rounded-2xl">
-
-                            </div>
-                        </div>
-                        <div className="min-w-40 h-full flex flex-col justify-center">
-                            <div className="bg-black w-full h-48 rounded-2xl mb-4">
-
-                            </div>
-                            <div className="bg-black w-full h-48 rounded-2xl">
+                            <div className="w-28 h-40 bg-black rounded-3xl">
 
                             </div>
                         </div>
-                        <div className="min-w-40 h-full flex items-center">
-                            <div className="bg-black w-full h-48 rounded-2xl">
+                        <div className="flex flex-col justify-center">
+                            <div className="w-28 h-40 bg-black rounded-3xl mb-4">
+
+                            </div>
+                            <div className="w-28 h-40 bg-black rounded-3xl">
+
+                            </div>
+                        </div>
+                        <div className="flex items-center">
+                            <div className="w-28 h-40 bg-black rounded-3xl ">
 
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="flex flex-col justify-center items-end w-1/2">
+                <div className="flex flex-col justify-center items-end">
                     <div className="flex flex-col justify-center items-end">
-                        <p>projeto</p>
-                        <h2>Frontend Fusion</h2>
-                        <p>Codifique o seu futuro hoje!</p>
+                        <p className="font-inconsolata text-gray-0  text-sizeParagraph font-medium">projeto</p>
+                        <h2 className="font-zen text-sizeTitle font-normal text-white">Frontend Fusion</h2>
+                        <p className="font-inconsolata text-gray-0 text-sizeParagraph text-s">Codifique o seu futuro hoje!</p>
                     </div>
-                    <div className="flex gap-4">
-                        <p>imagens</p>
-                        <p>imagens</p>
-                        <p>imagens</p>
-                        <p>imagens</p>
+                    <div className="flex gap-4 mt-8">
+                        <img src={react}/>
+                        <img src={vite}/>
+                        <img src={js}/>
+                        <img src={tailwind}/>
                     </div>
+
                 </div>
             </div>
         </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        zen: ["Zen Dots", "sans-serif"],
+        inconsolata: ["Inconsolata"],
+      },
+      textColor:{
+        gray: ["#B5B3BC"]
+      },
+      fontSize:{
+        sizeParagraph: ["24px"],
+        sizeTitle: ["38px"],
+      }
+    },
   },
   plugins: [],
 };
