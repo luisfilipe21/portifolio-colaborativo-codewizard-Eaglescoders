20236d0a2563c0622741600854a90307e4652b7a
Camila Silva <cf.silv466@gmail.com>
2024-06-05 23:31:40 -0300

    FEAT: add benefits/index.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { HeroSection } from "./components/hero";
 import { AboutProject } from "./components/about-project";
+import Benefits from "./components/benefits";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <main className="flex flex-col  bg-blueBg-0"> {/* Não mexer aqui por favor! */}
-       <HeroSection/>
-      <span>[Coloque aqui a sessão de benefits]</span>
+    <main className="flex flex-col  bg-blueBg-0">
+      {/* Não mexer aqui por favor! */}
+      <HeroSection />
+      <Benefits />
       <span>[Coloque aqui a sesão de tech-and-lang]</span>
       <span>[Coloque aqui a sesão de team]</span>
       <AboutProject />
       <span>[Coloque aqui a sesão de coments]</span>
-      <span>[Coloque aqui o cabecalho]</span> 
+      <span>[Coloque aqui o cabecalho]</span>
     </main>
   </React.StrictMode>
 );
