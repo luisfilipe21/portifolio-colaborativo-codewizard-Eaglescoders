e571ccacc8282facecec13c2af7f2e438ab2f693
Camila Silva <cf.silv466@gmail.com>
2024-06-14 14:26:18 -0300

    FEAT: add files and styles

--- src/components/benefits/card/index.jsx
+++ src/components/benefits/card/index.jsx
@@ -2,16 +2,16 @@ import PropTypes from "prop-types";
 
 const Card = ({ title, text, icon }) => {
   return (
-    <div className="">
-      <h2 className="">
+    <div className="bg-white-2 dark:bg-purple-2 drop-shadow-customized dark:drop-shadow-none rounded-lg flex flex-col gap-2 p-8   w-mini h-small lg:gap-4 lg:h-larger">
+      <h2 className="text-base font-bold flex gap-2.5  font-mulish text-purple-1 dark:text-gray-3 lg:text-lg lg:gap-1.5">
         <img
-          src={icon}
+          src={`./src/assets/icons/${icon}.svg`}
           alt="Icone do cartão"
-          className=""
+          className="h-6"
         />
         {title}
       </h2>
-      <p className="">
+      <p className="text-xs font-normal leading-mobile-medium text-purple-1 dark:text-purple-3 font-mulish lg:text-desktop-extraMini lg:leading-desktop-larger">
         {text}
       </p>
     </div>
