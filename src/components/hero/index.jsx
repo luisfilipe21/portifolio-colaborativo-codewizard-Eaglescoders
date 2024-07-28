63643baed0f55f1cbdebef176ef5b1c9774d4345
Davi Santanna <davicalvalcanti@gmail.com>
2024-07-27 21:06:54 -0300

    REFACTOR: " improving semantics"

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -2,68 +2,89 @@ import react from "../../assets/react.svg";
 import tailwind from "../../assets/tailwind.svg";
 import js from "../../assets/js.svg";
 import vite from "../../assets/vite.svg";
-import charmander from "../../assets/charmander.png";
+
 import useFetchData from "../../controller/useFetchData";
 
-
 export const HeroSection = () => {
+  const { data } = useFetchData(
+    "https://sistema-cadastro-dados-portifolio-front-end-fusion.vercel.app/api/EagleCoders"
+  );
 
-    const { data } = useFetchData('https://sistema-cadastro-dados-portifolio-front-end-fusion.vercel.app/api/EagleCoders');
+  return (
+    <section
+      className="w-full self-center gap-10  pb-16 items-center px-1 justify-center flex  flex-row items-center justify-between max-w-[1300px] flex-col-reverse
+      lg:flex-row
+      lg:pb-40 py-20
+      lg:px-10
+    "
+    >
+      <div className="flex  gap-2 md:gap-4">
+        <figure className="flex items-center justify-center ">
+          <img
+            src={data?.people[0].githubImgUrl}
+            alt={data?.people[0].name}
+            title={data?.people[0].name}
+            className="w-24  rounded-2xl md:w-32 "
+          />
+        </figure>
+        <figure className="flex  flex-col items-center justify-center gap-2 md:gap-4">
+          <img
+            src={data?.people[1].githubImgUrl}
+            alt={data?.people[1].name}
+            title={data?.people[1].name}
+            className="w-24   rounded-2xl md:w-32 "
+          />
+          <img
+            src={data?.people[2].githubImgUrl}
+            alt={data?.people[2].name}
+            title={data?.people[2].name}
+            className="w-24   rounded-2xl md:w-32 "
+          />
+        </figure>
+        <figure className="flex flex-col mt-6 items-center gap-2  md:gap-4">
+          <img
+            src={data?.people[3].githubImgUrl}
+            alt={data?.people[3].name}
+            title={data?.people[3].name}
+            className=" w-24  rounded-2xl md:w-32 "
+          />
+          <img
+            src={data?.people[4].githubImgUrl}
+            alt={data?.people[4].name}
+            title={data?.people[4].name}
+            className=" w-24    rounded-2xl md:w-32 "
+          />
+        </figure>
+        <figure className="flex mt-6  items-center justify-center">
+          <img
+            src={data?.people[4].githubImgUrl}
+            alt={data?.people[0].name}
+            title={data?.people[0].name}
+            className=" w-24    rounded-2xl md:w-32 "
+          />
+        </figure>
+      </div>
 
-    return (
-        <section className="w-full max-w-5xl mx-auto my-auto sm:max-w-md md:max-w-4xl lg:max-w-5xl">
-            <div className="flex flex-col-reverse items-center p-8 gap-8 md:flex-row md:justify-between">
-                <div className="h-full flex md:justify-center">
-                    <div className="flex items-center gap-2 lg:gap-4">
-                        <div className="flex">
-                            <div className="w-16 h-20 rounded-2xl mb-12 md:w-20 md:h-28 md:mb-34">
-                                <img src={data?.people[0].githubImgUrl} alt={data?.people[0].name} title={data?.people[0].name} className="w-full h-full rounded-2xl" />
-                            </div>
-                        </div>
-                        <div className="flex flex-col mb-16 md:mb-30">
-                            <div className="w-16 h-20 md:w-20 md:h-28 bg-black rounded-2xl mb-4">
-                                <img src={data?.people[1].githubImgUrl} alt={data?.people[1].name} title={data?.people[1].name} className="w-full h-full rounded-2xl" />
-                            </div>
-                            <div className="w-16 h-20 md:w-20 md:h-28 bg-black rounded-2xl">
-                                <img src={data?.people[2].githubImgUrl} alt={data?.people[2].name} title={data?.people[2].name} className="w-full h-full rounded-2xl" />
-                            </div>
-                        </div>
+      <div className="flex flex-col justify-center md:gap-4">
+        <div className="flex flex-col justify-center items-center md:items-end md:items-center">
+          <p className="font-inconsolata text-gray-0 font-medium text-xl md:text-sizeParagraph">
+            projeto
+          </p>
+          <h2 className="font-zen font-normal text-white text-[26px]  md:text-sizeTitle text-gray-0 ">
+            Frontend Fusion
+          </h2>
+          <p className="font-inconsolata text-gray-0 text-lg lg:text-sizeParagraph">
+            Codifique o seu futuro hoje!
+          </p>
+        </div>
 
-                        <div className="flex flex-col justify-center ">
-                            <div className="w-16 h-20 md:w-20 md:h-28 bg-black rounded-2xl mb-4">
-                                <img src={data?.people[3].githubImgUrl} alt={data?.people[3].name} title={data?.people[3].name} className="w-full h-full rounded-2xl" />
-                            </div>
-                            <div className="w-16 h-20 md:w-20 md:h-28 bg-black rounded-2xl">
-                                <img src={data?.people[4].githubImgUrl} alt={data?.people[4].name} title={data?.people[4].name} className="w-full h-full rounded-2xl" />
-                            </div>
-                        </div>
-
-                        <div className="flex justify-end">
-                            <div className="w-16 h-20 rounded-2xl mb-12 md:w-20 md:h-28 md:mb-34">
-                                <img src={data?.people[4].githubImgUrl} alt={data?.people[0].name} title={data?.people[0].name} className="w-full h-full rounded-2xl" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="flex flex-col justify-center md:gap-4">
-                    <div className="flex flex-col justify-center items-center md:items-end">
-                        <p className="font-inconsolata text-gray-0 font-medium text-lg lg:text-sizeParagraph">projeto</p>
-                        <h2 className="font-zen font-normal text-white text-3xl lg:text-sizeTitle text-gray-0">Frontend Fusion</h2>
-                        <p className="font-inconsolata text-gray-0 text-lg lg:text-sizeParagraph">Codifique o seu futuro hoje!</p>
-                    </div>
-
-                    <div className="flex justify-end">
-                        <div className="flex mt-8 gap-12 md:gap-4">
-                            <img src={react} />
-                            <img src={vite} />
-                            <img src={js} />
-                            <img src={tailwind} />
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </section>
-    )
-}
+        <div className="flex mt-6 gap-8 items-center self-center ">
+          <img src={react} className="w-10 md:w-14" />
+          <img src={vite} className="w-10 md:w-14" />
+          <img src={js} className="w-10 md:w-14" />
+          <img src={tailwind} className="w-10 md:w-14" />
+        </div>
+      </div>
+    </section>
+  );
+};
