7b803ebc95a2f7787175ca5884cd395d673bd049
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-08-01 09:58:06 -0300

    FEAT : backend for comments

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = required('express');
+const mongoose = required('mongoose');
+dotenv.config();
+
+const app = express();
+const PORT = process.dotenv.PORT || 5000;
+
+
+mongoose.connect(process.dotenv.MONGO_URI)
+.then(()=> console.log("Conectado"))
+.catch(err => console.error('falha ao conectar', err));
+
+app.use(express.json());
+
+app.get('/', getcomments)
+
