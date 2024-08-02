80edaa832e198775deddc0db85a0154d991518ca
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-08-02 17:28:23 -0300

    REFACTOR : Refactor some files

---
+++ src/components/coments/form/commentpolicy.jsx
@@ -0,0 +1,73 @@
+export const CommentPolicy = () => {
+  return (
+    <>
+
+<main className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white-1 px-6 py-8  text-[14px] w-[100%]">
+<section>
+        <header>
+          <h1>Política de Comentários do Projeto Frontend Fusion</h1>
+          <p>No Projeto Frontend Fusion, incentivamos a troca de ideias e o diálogo construtivo entre os membros da nossa comunidade. Para garantir um ambiente respeitoso e produtivo, estabelecemos as seguintes diretrizes para os comentários em nosso site:</p>
+          <ol>
+            <li>
+              <strong>Respeito e Civilidade</strong>
+              <ul>
+                <li>Todos os comentários devem ser respeitosos e educados.</li>
+                <li>Ofensas pessoais, assédio, discriminação ou qualquer forma de desrespeito não serão tolerados.</li>
+                <li>Críticas construtivas são bem-vindas, desde que sejam feitas de maneira respeitosa.</li>
+              </ul>
+            </li>
+            <li>
+              <strong>Pertinência</strong>
+              <ul>
+                <li>Os comentários devem estar relacionados ao tópico do post ou discussão.</li>
+                <li>Comentários fora do contexto ou que não agreguem valor ao tema serão removidos.</li>
+              </ul>
+            </li>
+            <li>
+              <strong>Spam e Publicidade</strong>
+              <ul>
+                <li>Comentários que contenham spam, publicidade ou autopromoção não são permitidos.</li>
+                <li>Links relevantes podem ser compartilhados, desde que contribuam para a discussão e não sejam meramente promocionais.</li>
+              </ul>
+            </li>
+            <li>
+              <strong>Conteúdo Inapropriado</strong>
+              <ul>
+                <li>Comentários que contenham linguagem ofensiva, conteúdo sexual, violento ou qualquer forma de discurso de ódio serão removidos.</li>
+                <li>Não publique informações pessoais suas ou de terceiros.</li>
+              </ul>
+            </li>
+            <li>
+              <strong>Direitos Autorais</strong>
+              <ul>
+                <li>Respeite os direitos autorais de terceiros. Não publique conteúdo protegido por direitos autorais sem a devida permissão.</li>
+              </ul>
+            </li>
+            <li>
+              <strong>Responsabilidade</strong>
+              <ul>
+                <li>Cada usuário é responsável pelo conteúdo que publica. Certifique-se de que seus comentários estão em conformidade com as leis aplicáveis.</li>
+              </ul>
+            </li>
+            <li>
+              <strong>Moderação e Pré-seleção</strong>
+              <ul>
+                <li>Todos os comentários serão pré-selecionados pela equipe do Projeto Frontend Fusion antes de serem publicados no site.</li>
+                <li>Comentários que não estejam de acordo com estas diretrizes serão editados ou removidos.</li>
+                <li>Comentários repetidamente ofensivos ou que violem esta política poderão resultar em suspensão ou banimento do usuário.</li>
+              </ul>
+            </li>
+            <li>
+              <strong>Feedback</strong>
+              <ul>
+                <li>Se você tiver dúvidas ou sugestões sobre nossa política de comentários, entre em contato conosco através do nosso formulário de contato.</li>
+              </ul>
+            </li>
+          </ol>
+          <p>Agradecemos por sua participação e por ajudar a manter nossa comunidade um lugar acolhedor e respeitoso para todos!</p>
+        </header>
+      </section>
+</main>
+    </>
+  );
+}
