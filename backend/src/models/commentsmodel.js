a513a62b2a0cdab658fa82f516fefc6006bb8048
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-08-03 11:50:01 -0300

    FIX : remove required rate prop

--- backend/src/models/commentsmodel.js
+++ backend/src/models/commentsmodel.js
@@ -5,7 +5,7 @@ const { Schema, model } = mongoose;
 const commentsSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  rate: { type: Number, required: true },
+  // rate: { type: Number, required: true },
   comment: { type: String, required: true },
   avatar : {type : String, required : false},
   githubuser : {type : String, required : false},
