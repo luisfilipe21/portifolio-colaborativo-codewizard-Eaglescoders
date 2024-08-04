f29bb6b9e2f3ae36185579f510d0656be8ec18d8
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-08-04 13:43:30 -0300

    FEAT : create vercel.json

--- backend/src/models/commentsmodel.js
+++ backend/src/models/commentsmodel.js
@@ -5,7 +5,6 @@ const { Schema, model } = mongoose;
 const commentsSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  // rate: { type: Number, required: true },
   comment: { type: String, required: true },
   avatar : {type : String, required : false},
   githubuser : {type : String, required : false},
