80910ff8ea90b0d22853634cedfad39c91cdd5b6
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-08-06 11:20:18 -0300

    fix : i changed de root path of the get method

--- backend/src/routes/commentsRoutes.js
+++ backend/src/routes/commentsRoutes.js
@@ -5,6 +5,6 @@ const { createAnewComment, getAllComments } = require('../controller/commentscon
 const router = express.Router();
 
 router.post('/send', createAnewComment);
-router.get('/', getAllComments)
+router.get('/get', getAllComments)
 
 module.exports = router;
