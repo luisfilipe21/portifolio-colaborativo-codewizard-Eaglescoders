c083155b34ed46cd4fe6a8436c88be111e677e0e
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-08-06 13:36:34 -0300

    TESTE : remove teste component

--- src/main.jsx
+++ src/main.jsx
@@ -9,14 +9,12 @@ import Comments from "./components/coments";
 import { Header } from "./components/cabecalho";
 import Team from "./components/team";
 import LingAndTech from "./components/ling-and-tech/LingAndTech";
-import { Teste } from "./components/coments/teste";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
 
     <main className="flex flex-col bg-purple-1"> {/* Não mexer aqui por favor! */}
-      <Teste />
       <Header />
       <HeroSection />
       <Benefits />
