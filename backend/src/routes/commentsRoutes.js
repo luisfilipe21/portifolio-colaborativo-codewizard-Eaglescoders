145719c374af97ec16fb3d1895c3f4bf2680e40f
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-08-06 10:26:06 -0300

    FIx : fix many files config

--- backend/src/routes/commentsRoutes.js
+++ backend/src/routes/commentsRoutes.js
@@ -5,6 +5,6 @@ const { createAnewComment, getAllComments } = require('../controller/commentscon
 const router = express.Router();
 
 router.post('/send', createAnewComment);
-router.get('/getcomments', getAllComments)
+router.get('/', getAllComments)
 
 module.exports = router;
